d51ed5ca7f545a54541dca95248f1f114ae0a53b
Sabir <93681489+sabircodes@users.noreply.github.com>
2023-10-17 01:26:01 +0530

    added some comments

--- A_Rigged.cpp
+++ A_Rigged.cpp
@@ -10,13 +10,16 @@ using namespace std;
          for(int i = 0 ; i < n ; i++){
              int s,e;
              cin>>s>>e;
+          // ye p ki strength and endurance
              int p_s,p_e;
+          // is step pe we have assumed ki p apni strength k equal weight utha  hi lega isliye ussey amswer declare krdia hai 
              if(i==0){
                   p_s = s;
                   p_e = e;
                  ans = s;
              }
              else {
+              // and in this part if we encounter some with greater or same strength && endurance that means ki p kabhi jeet nahi payega to ans=-1 krdia 
                  if(s>=p_s && e>=p_e){
                      ans=-1;
                  }
