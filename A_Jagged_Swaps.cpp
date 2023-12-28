18166b97cd291dcbb0dcd6dcadb78fe6bb95f97b
Sabir <93681489+sabircodes@users.noreply.github.com>
2023-12-28 22:09:49 +0530

    Update A_Jagged_Swaps.cpp

--- A_Jagged_Swaps.cpp
+++ A_Jagged_Swaps.cpp
@@ -1,34 +1,45 @@
-#include<iostream>
-#include<bits/stdc++.h>
+#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
-ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-     int t;
-     cin>>t;
-     while(t--){
-        int n ;
-        cin>>n;
-        vector<int>arr(n);
-        for(int i = 0 ; i < n ; i++){
-            cin>>arr[i];
-        
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
         }
-
-        vector<int>temp =arr;
-
-        sort(arr.begin(), arr.end());
-
-        for(int i =1; i < n -1 ; i++){
-            if(temp[i]>temp[i-1] && temp[i]>temp[i+1]){
-                swap(temp[i],temp[i+1]);
+        bool move = true;
+        while (move)
+        {
+            move = false;
+            for (int i = 1; i < n - 1; i++)
+            {
+                
+                if (arr[i - 1] < arr[i] && arr[i] > arr[i + 1])
+                {
+                    move = true;
+                    swap(arr[i], arr[i + 1]);
+                }
             }
         }
-        if(temp == arr){
-            cout<<"YES"<<endl;
+
+        if (is_sorted(arr.begin(), arr.end()))
+        {
+            cout << "YES" << endl;
         }
-        else{
-            cout<<"NO"<<endl;
+        else
+        {
+            cout << "NO" << endl;
         }
-}
-     return 0;
+    }
+    return 0;
 }
